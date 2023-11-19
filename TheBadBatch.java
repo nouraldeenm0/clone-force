f9ffab71832029dd3aee73158f1c61a1e53775f4
nouraldeenm0 <81125079+nouraldeenm0@users.noreply.github.com>
2023-11-19 08:29:10 +0000

    Initial Commit, TheBadBatch.java

---
+++ TheBadBatch.java
@@ -0,0 +1,30 @@
+/*
+ 
+{
+    "Omar Essameldin Mohamed 43-9989",
+    "Noureldin Mohamed 49-7269 T-20",
+    "Youssef ibrahim 49-7819"
+}
+
+ * Write your info here
+ * 
+ * @Team
+ * @name
+ * @id 
+ * @TutorialNumber
+ 
+*/
+
+package csen703.main.assignment1;
+
+public class TheBadBatch {
+	
+	public static boolean TatooineToNabooDivideAndConquer(int [] fuel) {
+		return false;
+	}
+	
+	public static boolean TatooineToNabooGreedy(int [] fuel) {
+		return false;
+	}
+
+}
