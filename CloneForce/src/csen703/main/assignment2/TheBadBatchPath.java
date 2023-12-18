9875d1ea4c5a8c692932b43ea74e33ca9adcb07c
nouraldeenm0 <81125079+nouraldeenm0@users.noreply.github.com>
2023-12-18 12:17:56 +0000

    implemeneted dp using dfs + hashMap memo

--- CloneForce/src/csen703/main/assignment2/TheBadBatchPath.java
+++ CloneForce/src/csen703/main/assignment2/TheBadBatchPath.java
@@ -19,42 +19,76 @@ package csen703.main.assignment2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class TheBadBatchPath {
+    int[] fuel = { 2, 3, 1, 1, 4 };
+    int[] fuel2 = { 9, 7, 7, 9, 8, 9, 9, 9, 9, 5, 5, 10, 7, 5, 8, 10, 7, 7, 10, 8, 5, 7, 5, 8, 8, 10, 6, 9, 5, 9, 7,
+        7, 7, 7, 6, 6, 7, 8, 10, 10, 8, 7, 6, 8, 7, 10, 6, 5, 7, 7, 8, 5, 8, 9, 10, 5, 7, 8, 7, 6, 10, 6, 9, 9,
+        7, 7, 5, 8, 8, 7, 8, 10, 9, 7, 9, 5, 9, 8, 5, 8, 5, 7, 8, 5, 8, 7, 7, 10, 7, 8, 9, 9, 8, 5, 6, 9, 8, 5,
+        9, 5, 9, 5, 6, 6, 10, 8, 7, 9, 5, 5, 8, 7, 8, 10, 9, 5, 6, 5, 10, 5, 8, 5, 6, 5, 8, 8, 7, 10, 10, 5, 7,
+        10, 10, 8, 5, 7, 6, 6, 10, 8, 10, 5, 8, 5, 5, 5, 10, 6, 9, 6, 5, 6, 6, 8, 8, 7, 7, 6, 6, 10, 7, 5, 8, 7,
+        6, 8, 9, 7, 5, 7, 6, 9, 10, 8, 10, 8, 8, 5, 8, 6, 5, 6, 8, 10, 5, 10, 9, 6, 6, 5, 8, 8, 9, 9, 5, 8, 8,
+        10, 10, 9, 5, 6, 8, 5, 10, 7, 7, 9, 5, 7, 5, 7, 9, 5, 7, 7, 6, 6, 6, 9, 10, 9, 5, 7, 8, 10, 9, 7, 6, 7,
+        10, 10, 5, 6, 7, 9, 10, 10, 9, 8, 5, 10, 10, 7, 10, 9, 6, 9, 5, 10, 5, 5, 8, 9, 6, 5, 7, 7, 5, 9, 7, 6,
+        5, 7, 8, 5, 9, 5, 7, 8, 5, 9, 8, 6, 5, 10, 8, 7, 6, 5, 8, 10, 10, 8, 7, 6, 5, 5, 10, 5, 9, 6, 9, 10, 7,
+        7, 10, 8, 7, 9, 9, 5, 6, 10, 10, 6, 9, 5, 5, 7, 10, 6, 10, 5, 5, 8, 9, 7, 5, 5, 7, 5, 7, 10, 6, 5, 9, 9,
+        7, 6, 10, 5, 8, 9, 8, 6, 8, 5, 8, 8, 9, 7, 5, 9, 9, 8, 10, 8, 9, 9, 5, 8, 5, 8, 9, 5, 9, 5, 7, 7, 6, 8,
+        10, 7, 6, 5, 5, 6, 7, 5, 5, 6, 8, 6, 10, 10, 7, 6, 9, 10, 10, 8, 10, 6, 7, 8, 5, 7, 8, 5, 8, 7, 5, 10,
+        7, 5, 8, 7, 5, 10, 7, 6, 10, 5, 7, 6, 8, 8, 7, 10, 5, 10, 10, 10, 6, 8, 8, 7, 5, 5, 10, 10, 6, 8, 9, 9,
+        10, 9, 6, 8, 5, 8, 7, 8, 8, 10, 7, 6, 8, 9, 7, 8, 7, 9, 6, 5, 10, 8, 9, 10, 5, 8, 6, 10, 6, 10, 10, 10,
+        7, 8, 10, 7, 8, 10, 5, 7, 9, 9, 5, 6, 8, 5, 10, 6, 7, 7, 6, 8, 5, 8, 9, 5, 10, 8, 8, 7, 10, 10, 9, 10,
+        9, 8, 7, 6, 8, 10, 10, 8, 5, 7, 7, 10, 9, 8, 9, 10, 6, 7, 5, 5, 10, 6, 6, 10, 9, 5, 7, 5, 9, 6, 7, 5, 9,
+        7, 10, 5, 6, 8, 7, 10, 5, 6, 7, 8, 7, 10, 8, 10, 6, 9, 6, 9, 6, 6, 5, 8, 8, 6, 9, 9, 6, 6, 6, 10, 6, 5,
+        7, 5, 7, 5, 10, 9, 8, 9, 5, 7, 5, 8, 10, 7, 5, 10, 5, 10, 8, 5, 7, 10, 9, 8, 8, 8, 8, 7, 8, 6, 9, 6, 6,
+        8, 9, 7, 6, 9, 8, 10, 5, 5, 10, 6, 9, 10, 10, 8, 5, 9, 10, 6, 10, 9, 5, 6, 5, 10, 9, 9, 7, 9, 6, 5, 7,
+        6, 8, 7, 9, 7, 7, 6, 6, 7, 8, 5, 8, 10, 7, 5, 10, 6, 9, 5, 10, 8, 5, 9, 7, 10, 6, 6, 6, 8, 9, 9, 5, 8,
+        5, 8, 8, 6, 7, 8, 7, 10, 9, 7, 8, 9, 9, 10, 8, 10, 10, 9, 5, 7, 5, 10, 9, 6, 8, 6, 9, 10, 9, 6, 9, 6, 6,
+        8, 9, 8, 8, 8, 9, 7, 8, 6, 8, 6, 8, 5, 8, 9, 8, 6, 9, 9, 6, 7, 6, 7, 5, 8, 8, 10, 8, 8, 5, 6, 9, 9, 5,
+        7, 7, 5, 5, 6, 9, 9, 10, 5, 10, 9, 7, 10, 7, 7, 5, 10, 5, 8, 9, 6, 8, 10, 8, 8, 6, 6, 9, 8, 10, 8, 9, 5,
+        8, 5, 6, 5, 6, 10, 5, 5, 6, 10, 10, 10, 9, 8, 8, 5, 8, 5, 7, 7, 8, 9, 9, 10, 5, 6, 10, 7, 8, 10, 5, 9,
+        10, 5, 5, 9, 7, 5, 9, 5, 10, 7, 6, 10, 9, 7, 9, 6, 8, 6, 7, 9, 7, 6, 10, 8, 6, 10, 9, 5, 6, 9, 6, 7, 7,
+        10, 8, 8, 7, 9, 5, 8, 9, 7, 10, 10, 10, 5, 5, 6, 6, 10, 5, 5, 7, 10, 10, 10, 6, 10, 5, 7, 5, 9, 9, 6, 7,
+        5, 10, 8, 10, 8, 8, 7, 6, 6, 7, 9, 6, 5, 5, 9, 6, 7, 6, 10, 5, 5, 9, 7, 9, 5, 7, 10, 6, 10, 5, 10, 7, 8,
+        7, 6, 7, 10, 9, 6, 10, 8, 7, 6, 9, 6, 10, 6, 6, 7, 7, 9, 9, 8, 6, 6, 10, 5, 7, 7, 7, 10, 10, 5, 6, 8, 8,
+        7, 6, 7, 8, 10, 10, 8, 5, 9, 5, 7, 7, 6, 7, 8, 7, 7, 8, 6, 7, 7, 10, 5, 9, 8, 6, 8, 8, 5, 7, 6, 8, 9,
+        10, 5, 7, 10, 8, 6, 9, 8, 6, 7, 5, 8, 6, 6, 6, 6, 6, 10, 8, 8, 10, 7, 5, 8, 8, 10, 8, 6, 7, 9, 5, 9, 9,
+        9, 7, 5, 9, 7, 10, 8, 7, 9, 6, 6, 7, 5, 9, 9, 5, 10 };
+    HashMap<Integer, Integer> memo = new HashMap<>();
 
-/*
-After Tech managed to find out that they would be able to reach planet Naboo from
-Tatooine, he realised that they needed to be as discrete as possible. To do that Tech
-decided to minimize their landings as much as possible. You need to help Tech to:
-    a) Design a Dynamic programming algorithm that outputs the minimum number of
-planets the Bad Batch needs to land on and fuel to reach Naboo in O(n^2).
-    b) Reconsruct a path that leads to Naboo with that minimum number of landings in O(n^2)
-*/
     public static void main(String[] args) {
-        int[] fuel = { 2, 3, 1, 1, 4 };
-        System.out.println("Output DP: " + TatooineToNabooDP(fuel));
-        System.out.println("Output Path: " + TatooineToNabooPath(fuel));
+        var obj = new TheBadBatchPath();
+        obj.TatooineToNabooDP(obj.fuel);
+
+        System.out.println("Output DP: " + obj.TatooineToNabooDP(obj.fuel2));
+        // System.out.println("Output Path: " + TatooineToNabooPath(fuel));
     }
 
-    /*
-    a dynamic programming approach to finding the minimum number of landings to
-reach planet Naboo. Notice that you always land on Naboo anyway.
-     */
-    public static int TatooineToNabooDP(int[] fuel) {
-        if (fuel[0] == fuel.length - (0 + 1))
-            return 1;
-        else {
-            return TatooineToNabooDP(Arrays.copyOfRange(fuel, 1, fuel.length));
+    // this is using dfs
+    public int TatooineToNabooDP(int[] fuel) {
+        if (this.memo.containsKey(fuel.length))
+            return this.memo.get(fuel.length);
+
+        if (fuel.length == 1)
+            return 0;
+
+        int min = Integer.MAX_VALUE;
+        int lastIndx = fuel.length - 1;
+
+        for (int i = 0; i <= fuel.length - 2; ++i) {
+            int distToEnd = lastIndx - i;
+            int fuelInPlanet = fuel[i];
+
+            if (fuelInPlanet >= distToEnd) {
+                int[] subArray = Arrays.copyOfRange(fuel, 0, i + 1);
+                min = Math.min(TatooineToNabooDP(subArray) + 1, min);
+            }
         }
-        // return 0;
+        this.memo.put(fuel.length, min);
+        return min;
     }
 
-    /*
-    returns an ordered minimum path of the planets indices the BadBatch should land
-on to reach Naboo. Notice that Tatooine and Naboo are always the start and end
-of this path. If there are multiple minimum paths, you only return one of them.
-    */
-    public static ArrayList<Integer> TatooineToNabooPath(int[] fuel) {
+    public ArrayList<Integer> TatooineToNabooPath(int[] fuel) {
         return new ArrayList<>(Arrays.asList(0, 1, 4));
     }
 }
